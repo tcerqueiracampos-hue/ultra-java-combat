02204fd370916e02bd693fe8f26d9dd6c0f72867
Thalyta C. Campos <tcerqueiracampos@gmail.com>
2026-06-23 13:01:42 -0300

    Add files via upload

---
+++ JogoUfc.java
@@ -0,0 +1,10 @@
+public class JogoUfc {
+    public static void main(String[] args) {
+        // Agora o Java encontra a classe na mesma página sem problemas!
+        Personagens L[] = new Personagens[6];
+        
+       l[0]= new ("PretyBoy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+        
+        L[0].status();
+    }
+}
